2f2d130e50e827e10a884c4b10f6bee193da514c
Michael Rojas <rojas.michael@hotmail.com>
2016-01-18 14:56:54 -0500

    Iteration 3

--- ClassWork/week_2/Day6/Lexiconomitron/lib/Lexiconomitron.rb
+++ ClassWork/week_2/Day6/Lexiconomitron/lib/Lexiconomitron.rb
@@ -1,22 +1,34 @@
 class Lexiconomitron
 
 	def eat_t(string)
-     if string.is_a? String
+     	if string.is_a? String
     	string.gsub(/[tT]/,'')
-    else
+    	else
     	nil
-  end 
+  		end 
 	end 
 
-	 def shazam (array)
+	def shazam (array)
     	if array.is_a? Array
    		 array.map do |word| 
-      	eat_t(word).reverse!
+      		eat_t(word).reverse!
    		 end
 		else 
 			[]
+		end
+ 	end
+
+ 	def oompa_loompa(array)
+ 		if array.is_a? Array 
+ 		 	array_no_t = array.map do |word|
+ 		 		eat_t(word)
+ 		 	end
+
+ 		 	array_no_t.delete_if { |word| word.length > 3 }
  		 end
-end
+ 	end
+
+
 end
 
 # lexi = Lexiconomitron.new
--- ClassWork/week_2/Day6/Lexiconomitron/spec/Lexiconomitron_spec.rb
+++ ClassWork/week_2/Day6/Lexiconomitron/spec/Lexiconomitron_spec.rb
@@ -11,7 +11,7 @@ describe Lexiconomitron do
 	 
   describe "#eat_t" do
     it "removes every letter t from the input" do
-      expect(@lexi.shazam(["great", "scott", "!"])).to eq(["aerg", "ocs", "!"])
+      expect(@lexi.oompa_loompa(["if", "you", "wanna", "be", "my", "lover"])).to eq(["if", "you", "be", "my"])
     end
    #  it "does not remove a t when there is no t" do
    #  	expect(@lexi.eat_t("Hey You!")).to eq("Hey You!")
