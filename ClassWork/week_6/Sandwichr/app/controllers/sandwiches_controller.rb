42d9d038b676e4a8bdf45d59cc76f873ef6121a5
Michael Rojas <rojas.michael@hotmail.com>
2016-02-15 15:29:54 -0500

    Sandwich version 1 & 2 completed

---
+++ ClassWork/week_6/Sandwichr/app/controllers/sandwiches_controller.rb
@@ -0,0 +1,58 @@
+class SandwichesController < ApplicationController
+
+	def index
+			sandwiches = Sandwich.all
+			render json: sandwiches
+	end
+
+	def create
+	
+		sandwich = Sandwich.create(sandwich_params)
+		render json: sandwich
+	end
+
+	def show
+		sandwich = Sandwich.find_by(id: params[:id])
+		unless sandwich
+				render json: {error: "sandwich not found"},
+				status: 404
+				return
+		end
+		render json: sandwich
+	end
+
+
+	def update
+		sandwich = Sandwich.find_by(id: params[:id])
+		unless sandwich
+				render json: {error: "sandwich not found"},
+				status: 404
+				return
+		end
+		sandwich.update(sandwich_params)
+		render json: sandwich
+	end
+
+	def destory
+		sandwich = Sandwich.find_by(id: params[:id])
+		unless sandwich
+				render json: {error: "sandwich not found"},
+				status: 404
+				return
+		end
+		sandwich.destory
+		render json: sandwich
+
+
+	
+	end
+
+
+	private
+
+	def sandwich_params
+		params.require(:sandwich).permit(:name, :bread_type)
+
+		
+	end
+end
